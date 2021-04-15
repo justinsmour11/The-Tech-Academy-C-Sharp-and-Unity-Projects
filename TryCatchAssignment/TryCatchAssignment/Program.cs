25ca3c488880d00525f689ffd1f629e1fbb1478b
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-14 17:52:54 -0700

    Try Catch Assignment

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int>() { 1, 6, 7, 43, 68, 79 };
+                Console.WriteLine("Please enter a whole number");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number / userInput);
+                }
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("We've moved past the issue");
+            }
+            Console.WriteLine("We've continued on with program execution");
+            Console.ReadLine();
+        }
+    }
+}
