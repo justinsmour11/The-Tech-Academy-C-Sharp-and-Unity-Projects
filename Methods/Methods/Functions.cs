8f384e1e179fac468547ee0df002d5e9b8a90a9d
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-17 12:52:08 -0700

    Calling methods assignment

---
+++ Methods/Methods/Functions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Functions
+    {
+        public static int Multiply(int userInput)
+        {
+            int multAnswer = userInput * 4;
+            return multAnswer;
+        }
+        public static int Divide(int userInput)
+        {
+            int divisionAnswer = userInput / 2;
+            return divisionAnswer;
+        }
+        public static int Fraction(int userInput)
+        {
+            int fractionAnswer = userInput % 10;
+            return fractionAnswer;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a value to perform math on: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int multiply = Functions.Multiply(userInput);
+            int divide = Functions.Divide(userInput);
+            int fraction = Functions.Fraction(userInput);
+            Console.WriteLine("Your number times 4 equals: " + multiply);
+            Console.ReadLine();
+            Console.WriteLine("Your number divided by 2 equals: " + divide);
+            Console.ReadLine();
+            Console.WriteLine("The remainder after your number is divided by 10 is: "  + fraction);
+            Console.ReadLine();
+        }
+    }
+}
