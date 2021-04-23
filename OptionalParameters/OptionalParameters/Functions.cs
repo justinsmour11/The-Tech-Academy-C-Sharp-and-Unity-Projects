9691085eefc48ef9ac9c864078a75a21056e4a8b
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-23 14:10:53 -0700

    Optional Parameters

---
+++ OptionalParameters/OptionalParameters/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    public class Functions
+    {
+        public int Function(int inputOne, int inputTwo = 2)
+        {
+            int numAnswer = inputOne * inputTwo;
+            return numAnswer;
+        }
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Functions function = new Functions();
+
+            Console.WriteLine("Please enter a number to perform math on");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may enter another number (this is optional)");
+            int inputTwo;
+
+            if(int.TryParse(Console.ReadLine(), out inputTwo))
+            {
+                Console.WriteLine(function.Function(inputOne, inputTwo));
+            }
+            else
+            {
+                Console.WriteLine(function.Function(inputOne));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
