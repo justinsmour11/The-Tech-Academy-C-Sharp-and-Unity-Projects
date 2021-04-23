77ff3b5c15ea5556f718d02cee7867811f1135a6
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-23 14:12:09 -0700

    Void method

---
+++ VoidMethod/VoidMethod/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Functions
+    {
+        public int mathOne { get; set; }
+
+        public void Function(int inputOne, int inputTwo, out int mathOne)
+        {
+            mathOne = inputOne * 15;
+            Console.WriteLine(inputTwo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions function = new Functions();
+            function.Function(2, 70, out int mathOne);
+            Console.WriteLine(mathOne);
+            Console.ReadLine();
+            int inputOne = 4;
+            int inputTwo = 80;
+            function.Function(inputOne, inputTwo, out mathOne);
+            Console.WriteLine(mathOne);
+            Console.ReadLine();
+        }
+    }
+}
