77f62fec59dc6937c4a175e4c7c88496d4d8d000
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-22 21:33:34 -0700

    Overloading methods

---
+++ OverLoadingMethods/OverLoadingMethods/Functions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadingMethods
+{
+    public class Functions
+    {
+        public Functions()
+        {
+            Console.WriteLine("It is working");
+        }
+
+        public int Function(int userInput)
+        {
+            int multAnswer = userInput * 50;
+            return multAnswer;
+        }
+
+        
+
+        public decimal Function(decimal userDecimal)
+        {
+            decimal multDec = userDecimal * 35;
+            return multDec;
+        }
+
+        public int Function(string userString)
+        {
+            int strConvert = Convert.ToInt32(userString);
+            int strConvertAnswer = strConvert + 100;
+            return strConvertAnswer;
+        }
+
+        public int userInput { get; set; }
+        public decimal userDecimal { get; set; }
+        public string userString { get; set; }
+    }
+}
---
+++ OverLoadingMethods/OverLoadingMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadingMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 25;
+            decimal userDecimal = 45.00m;
+            string userString = "9";
+
+            Functions function = new Functions();
+            function.userInput = userInput;
+            function.userDecimal = userDecimal;
+
+
+
+
+
+            Console.WriteLine(function.Function(userInput));
+            Console.WriteLine(function.Function(userDecimal));
+            Console.WriteLine(function.Function(userString));
+            Console.ReadLine();
+        }
+    }
+}
