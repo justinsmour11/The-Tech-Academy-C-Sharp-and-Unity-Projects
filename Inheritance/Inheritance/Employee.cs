d02111fb6ab1a87fcee21eb32c11267663b0b091
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-24 11:56:51 -0700

    Inheritance

---
+++ Inheritance/Inheritance/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Employee<T> : Person
+    {
+        public List<T> Things { get; set; }
+
+        public int ID { get; set; }
+
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    return (employee1.ID == employee2.ID);
+        //}
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return !(employee1.ID == employee2.ID);
+        //}
+    }
+    
+    
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee1 = new Employee() { FirstName = "Sample", LastName = "Student", ID=1 };
+            //employee1.SayName();
+            //Employee employee2 = new Employee() { FirstName = "Justin", LastName = "Seymour", ID = 1 };
+
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string>() { "Justin", "Bob", "Tonya" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>() { 31, 45, 37 };
+
+            foreach(string name in employee1.Things)
+            {
+                Console.WriteLine(name);
+            }
+            foreach(int age in employee2.Things)
+            {
+                Console.WriteLine(age);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
