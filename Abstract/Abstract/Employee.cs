ef9498b9ade61db6171e30698cd2baf022e44ddd
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-04-24 13:16:38 -0700

    Abstract

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        List<T> things = new List<T>();
+
+        public override void SayName()
+        {
+            Console.WriteLine("{0} {1}", firstName, lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} quits the game", firstName, lastName);
+        }
+    }
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            //employee.SayName();
+            //Console.ReadLine();
+
+            //IQuittable quit = new Employee() { firstName = "Justin", lastName = "Seymour" };
+            //quit.Quit();
+            //Console.ReadLine();
+
+            
+            
+        }
+    }
+}
