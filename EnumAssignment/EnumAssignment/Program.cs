8c43857b058659de2ea6f6c953dd23abd5283398
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2021-05-04 13:48:10 -0700

    Enums

---
+++ EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week week = new Week();
+            week.DayOfWeek = new List<DayOfWeek>();
+                        
+            for (int i = 0; i < 7; i++)
+            {
+                week.DayOfWeek.Add((DayOfWeek)i);
+            }
+            
+            Console.WriteLine("Please enter a day of the week");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                DayOfWeek userDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), userInput);
+                
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ EnumAssignment/EnumAssignment/Week.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Week
+    {
+        public List<DayOfWeek> DayOfWeek { get; set; }
+    }
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
